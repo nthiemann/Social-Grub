5b987f6431458198c67d705b9620d78970e00c38
Allec Pereira <alleccpereira@gmail.com>
2021-02-11 18:16:57 -0500

    created sign in and register page

---
+++ app/src/main/java/com/example/socialgrub/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.socialgrub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button registerBtn;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button registerBtn = (Button) findViewById(R.id.register_button);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=  new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        }
+    }
---
+++ app/src/main/java/com/example/socialgrub/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.socialgrub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    // change password to text password
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
