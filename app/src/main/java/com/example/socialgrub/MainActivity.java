325cecee0ea694489cd20ab96497eae84b9e90dd
Allec Pereira <alleccpereira@gmail.com>
2021-02-14 22:34:08 -0500

    missing gradle files

--- app/src/main/java/com/example/socialgrub/MainActivity.java
+++ app/src/main/java/com/example/socialgrub/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=  new Intent(MainActivity.this,RegisterActivity.class);
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
                 startActivity(intent);
             }
         });
