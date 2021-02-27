bcb2f673b65e5d84ec5caba8c5d90c760be92cb9
Allec Pereira <alleccpereira@gmail.com>
2021-02-27 00:57:00 -0500

    The user can now select images from the gallery to set as their profile picture

--- app/src/main/java/com/example/socialgrub/ProfileActivity.java
+++ app/src/main/java/com/example/socialgrub/ProfileActivity.java
@@ -2,13 +2,34 @@ package com.example.socialgrub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class ProfileActivity extends AppCompatActivity {
-
+    Button editProfileBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        editProfileBtn = (Button) findViewById(R.id.edit_profile_button);
+
+        editProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToeditPage = new Intent(ProfileActivity.this, EditProfileActivity.class);
+                startActivity(goToeditPage);
+            }
+        });
+
+
+
+
+
+
+
     }
 }
