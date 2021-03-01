de133797f90e4e5012ebe322d713d9664eaf20ac
kylekaracadag <kylekaracadag1@gmail.com>
2021-02-28 19:00:04 -0500

    Added functionality so be able to view your own posts in your profile page

--- app/src/main/java/com/example/socialgrub/Recipe.java
+++ app/src/main/java/com/example/socialgrub/Recipe.java
@@ -5,10 +5,89 @@ import java.util.ArrayList;
 public class Recipe
 {
     public String recipeTitle;
-    public String description;
-    public Recipe(String title, String description)
-    {
+    public String recipeDescription;
+    public String recipeTagOne;
+    public String recipeTagTwo;
+    public String recipeTagThree;
+    public ArrayList<String> recipeIngredientList;
+    public ArrayList<String> recipeExtraTags;
+    public ArrayList<String> recipeMeasurements;
 
+    public Recipe(String recipeTitle, String recipeDescription, String recipeTagOne,
+                  String recipeTagTwo, String recipeTagThree,
+                  ArrayList<String> recipeIngredientList, ArrayList<String> recipeExtraTags,
+                  ArrayList<String> recipeMeasurements) {
+        this.recipeTitle = recipeTitle;
+        this.recipeDescription = recipeDescription;
+        this.recipeTagOne = recipeTagOne;
+        this.recipeTagTwo = recipeTagTwo;
+        this.recipeTagThree = recipeTagThree;
+        this.recipeIngredientList = recipeIngredientList;
+        this.recipeExtraTags = recipeExtraTags;
+        this.recipeMeasurements = recipeMeasurements;
     }
 
+    public String getRecipeTitle() {
+        return recipeTitle;
+    }
+
+    public void setRecipeTitle(String recipeTitle) {
+        this.recipeTitle = recipeTitle;
+    }
+
+    public String getRecipeDescription() {
+        return recipeDescription;
+    }
+
+    public void setRecipeDescription(String recipeDescription) {
+        this.recipeDescription = recipeDescription;
+    }
+
+    public String getRecipeTagOne() {
+        return recipeTagOne;
+    }
+
+    public void setRecipeTagOne(String recipeTagOne) {
+        this.recipeTagOne = recipeTagOne;
+    }
+
+    public String getRecipeTagTwo() {
+        return recipeTagTwo;
+    }
+
+    public void setRecipeTagTwo(String recipeTagTwo) {
+        this.recipeTagTwo = recipeTagTwo;
+    }
+
+    public String getRecipeTagThree() {
+        return recipeTagThree;
+    }
+
+    public void setRecipeTagThree(String recipeTagThree) {
+        this.recipeTagThree = recipeTagThree;
+    }
+
+    public ArrayList<String> getRecipeIngredientList() {
+        return recipeIngredientList;
+    }
+
+    public void setRecipeIngredientList(ArrayList<String> recipeIngredientList) {
+        this.recipeIngredientList = recipeIngredientList;
+    }
+
+    public ArrayList<String> getRecipeExtraTags() {
+        return recipeExtraTags;
+    }
+
+    public void setRecipeExtraTags(ArrayList<String> recipeExtraTags) {
+        this.recipeExtraTags = recipeExtraTags;
+    }
+
+    public ArrayList<String> getRecipeMeasurements() {
+        return recipeMeasurements;
+    }
+
+    public void setRecipeMeasurements(ArrayList<String> recipeMeasurements) {
+        this.recipeMeasurements = recipeMeasurements;
+    }
 }
