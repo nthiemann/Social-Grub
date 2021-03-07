4aaabd424aa8ddfa90db639202e2809c5f7b0ae4
Allec Pereira <alleccpereira@gmail.com>
2021-03-07 17:28:14 -0500

    User profile images now save to the account

--- app/src/main/java/com/example/socialgrub/EditProfileActivity.java
+++ app/src/main/java/com/example/socialgrub/EditProfileActivity.java
@@ -4,17 +4,30 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.PermissionChecker;
-
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -24,69 +37,76 @@ public class EditProfileActivity extends AppCompatActivity {
     Button changeButton;
     final int IMG_PICK = 1;
     final int PERMISSION__CODE = 2;
+    StorageReference storageReference;
+    FirebaseAuth fAuth;
+    private int progr = 0;
+    Button increment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
+
+
         profileImage = findViewById(R.id.profilepic_imageView);
         changeButton = findViewById(R.id.change_button);
+        fAuth = FirebaseAuth.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+        StorageReference profileRef = storageReference.child("users/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
+
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Glide.with(EditProfileActivity.this).load(uri).into(profileImage);
+            }
+        });
 
         changeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-                        String [] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
-                        requestPermissions(permissions,PERMISSION__CODE);
-                    }
-                    else {
-                        pickImageFromGallery();
-                    }
-                }
-                // sdk is less than marshmellow
-                else{
-                    pickImageFromGallery();
-                }
+
+                // content uri from image in mediastore is selected
+                Intent openGallery = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGallery,IMG_PICK);
             }
-
-
         });
-    }
-    private void pickImageFromGallery(){
-        Intent gallery = new Intent(Intent.ACTION_PICK);
-        startActivityForResult(gallery,IMG_PICK);
-        gallery.setType("image/*");
-        startActivityForResult(gallery,IMG_PICK);
 
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        switch (requestCode){
-            case PERMISSION__CODE: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    pickImageFromGallery();
-
-                }
-                // permission was denied!
-                else {
-                    Toast.makeText(EditProfileActivity.this, "Permission denied", Toast.LENGTH_LONG).show();
-                }
-            }
-        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK && requestCode == IMG_PICK){
-            profileImage.setImageURI(data.getData());
+        if (requestCode == IMG_PICK){
+            if (resultCode == Activity.RESULT_OK){
+                Uri imageUri = data.getData();
 
+                //profileImage.setImageURI(imageUri);
+                uploadToFirebase(imageUri);
+            }
         }
     }
 
+    private void uploadToFirebase(Uri imageUri){
+        StorageReference fileRef = storageReference.child("users/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Glide.with(EditProfileActivity.this).load(uri).into(profileImage);
+
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(EditProfileActivity.this,"failed",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
