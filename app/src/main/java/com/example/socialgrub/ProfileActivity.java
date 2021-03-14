d76038426bb6cab8b1b9d68833a573ea203e87e9
kylekaracadag <kylekaracadag1@gmail.com>
2021-03-14 17:25:26 -0400

    Testing recycler view

--- app/src/main/java/com/example/socialgrub/ProfileActivity.java
+++ app/src/main/java/com/example/socialgrub/ProfileActivity.java
@@ -1,15 +1,49 @@
 package com.example.socialgrub;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 
 public class ProfileActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    RecyclerView recyclerView;
+
+    String title[] = {"Test1", "Test2", "Test3", "Test4", "Test5"};
+
+//    DatabaseReference mRef = FirebaseDatabase.getInstance().getReference().child("-MVDXb5bcZhkXKVr7R-1");
+
+//    RecyclerView recyclerView;
+//    List<Recipe> recipeList;
+//    RecipeAdapter recipeAdapter;
+//
+//    String s1[] = {"Title1"}, s2[] = {"Tag1"}, s3[] = {"Tag2"}, s4[] = {"Tag3"};
+//    int images[] = {R.drawable.profile_image};
+//    String images[] = {"https://firebasestorage.googleapis.com/v0/b/social-grub.appspot.com/o/Posts%2F1615151653991.null?alt=media&token=50794e2c-2c8b-462a-af1a-5ae19ee51e9b",
+//            "https://firebasestorage.googleapis.com/v0/b/social-grub.appspot.com/o/Posts%2F1615152198286.null?alt=media&token=9405c171-64cf-4109-be45-ef4da8a11ca0",
+//            "https://firebasestorage.googleapis.com/v0/b/social-grub.appspot.com/o/Posts%2F1615152307525.null?alt=media&token=9cce337b-ab19-496f-9e00-6db56016e3fb",
+//            "https://firebasestorage.googleapis.com/v0/b/social-grub.appspot.com/o/Posts%2F1615152401672.null?alt=media&token=f615cd7b-1af4-4266-b367-06cffd283b79",
+//            "https://firebasestorage.googleapis.com/v0/b/social-grub.appspot.com/o/Posts%2F1615155237697.null?alt=media&token=dedc749b-5852-4d00-921c-8b79b094e33e",
+//            "https://firebasestorage.googleapis.com/v0/b/social-grub.appspot.com/o/Posts%2F1615155531669.null?alt=media&token=3849bec2-ad56-496c-bf84-48e5c49862a8",
+//            "https://firebasestorage.googleapis.com/v0/b/social-grub.appspot.com/o/Posts%2F1615155689162.null?alt=media&token=d93bba6f-cef0-4331-b10d-0c854d91b28f"};
+
     Button editProfileBtn;
     ImageButton settingsProfileBtn;
 
@@ -17,6 +51,8 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        recyclerView = findViewById(R.id.profilePostView);
 
         editProfileBtn = (Button) findViewById(R.id.edit_profile_button);
 
@@ -37,11 +73,17 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        RecipeAdapter recipeAdapter = new RecipeAdapter(this, title);
+        recyclerView.setAdapter(recipeAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-
-
-
-
-
+//        recyclerView = findViewById(R.id.profilePostView);
+////        s1 = ;
+////        s2 = ;
+////        s3 = ;
+////        ImageView view1 = Picasso.get().load("http://i.imgur.com/DvpvklR.png");
+//        RecipeAdapter recipeAdapter = new RecipeAdapter(this, s1, s2, s3, s4, images);
+//        recyclerView.setAdapter(recipeAdapter);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/example/socialgrub/RecipeAdapter.java
+++ app/src/main/java/com/example/socialgrub/RecipeAdapter.java
@@ -1,6 +1,7 @@
 package com.example.socialgrub;
 
 import android.content.Context;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
@@ -18,69 +20,132 @@ import java.util.List;
 
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.Holder>{
 
+    String tTitle[];
     Context context;
-    List<Recipe> profilePostList;
 
-    public RecipeAdapter(Context context, List<Recipe> profilePostList) {
-        this.context = context;
-        this.profilePostList = profilePostList;
+    public RecipeAdapter(Context ct, String thisTitle[])
+    {
+        context = ct;
+        tTitle = thisTitle;
     }
 
     @NonNull
     @Override
-    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.activity_profile, parent, false);
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.recycler_test, parent, false);
         return new Holder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull Holder holder, int position) {
-        String title = profilePostList.get(position).getRecipeTitle();
-        String tag1 = profilePostList.get(position).getRecipeTagOne();
-        String tag2 = profilePostList.get(position).getRecipeTagTwo();
-        String tag3 = profilePostList.get(position).getRecipeTagThree();
-        String image = profilePostList.get(position).getRecipeUrl();
-
-        holder.postTitle.setText(title);
-        holder.tagOne.setText(tag1);
-        holder.tagTwo.setText(tag2);
-        holder.tagThree.setText(tag3);
-
-        try {
-            Picasso.get().load(image).into(holder.postImage);
-        }
-        catch(Exception e) {
-
-        }
-
-        holder.viewPostButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, "View Post", Toast.LENGTH_SHORT).show();
-            }
-        });
+    public void onBindViewHolder(@NonNull RecipeAdapter.Holder holder, int position) {
+//        holder.nameTxt.setText(tTitle[position]);
+        holder.nameTxt.setText(tTitle[0]);
     }
 
     @Override
     public int getItemCount() {
-        return profilePostList.size();
+        return tTitle.length;
     }
 
-    class Holder extends RecyclerView.ViewHolder{
+    public class Holder extends RecyclerView.ViewHolder {
+        TextView nameTxt;
 
-        ImageView postImage;
-        TextView tagOne, tagTwo, tagThree, postTitle;
-        Button viewPostButton;
-
-        public Holder(@NonNull View itemView){
-            super(itemView);
-
-            postImage = itemView.findViewById(R.id.profileRecipeImage);
-            tagOne = itemView.findViewById(R.id.profileRecipeTag1);
-            tagTwo = itemView.findViewById(R.id.profileRecipeTag2);
-            tagThree = itemView.findViewById(R.id.profileRecipeTag3);
-            postTitle = itemView.findViewById(R.id.profileRecipeTitle);
-            viewPostButton = itemView.findViewById(R.id.profileRecipeViewPostButton);
+        public Holder(final View view){
+            super(view);
+            nameTxt = view.findViewById(R.id.testTitle);
         }
     }
+//    Context context;
+//    List<Recipe> profilePostList;
+//
+////    public RecipeAdapter(Context context, List<Recipe> profilePostList) {
+////        this.context = context;
+////        this.profilePostList = profilePostList;
+////    }
+//
+//    String data1[], data2[], data3[], data4[];
+//    int images[];
+//
+//    public RecipeAdapter(Context context, String s1[], String s2[], String s3[], String s4[], int img[]) {
+//        this.context = context;
+//        data1 = s1;
+//        data2 = s2;
+//        data3 = s3;
+//        data4 = s4;
+//        images = img;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+////        View view = LayoutInflater.from(context).inflate(R.layout.profile_posts, parent, false);
+//        LayoutInflater layoutInflater = LayoutInflater.from(context);
+//        View view = layoutInflater.inflate(R.layout.profile_posts, parent, false);
+//
+////        LinearLayoutManager layoutManager = new LinearLayoutManager(
+////                getActivity());
+////        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+////        mRecyclerView.setLayoutManager(layoutManager);
+//
+//        return new Holder(view);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull Holder holder, int position) {
+////        String title = profilePostList.get(position).getRecipeTitle();
+////        String tag1 = profilePostList.get(position).getRecipeTagOne();
+////        String tag2 = profilePostList.get(position).getRecipeTagTwo();
+////        String tag3 = profilePostList.get(position).getRecipeTagThree();
+////        String image = profilePostList.get(position).getRecipeUrl();
+////
+////        holder.postTitle.setText(title);
+////        holder.tagOne.setText(tag1);
+////        holder.tagTwo.setText(tag2);
+////        holder.tagThree.setText(tag3);
+////
+////        try {
+////            Picasso.get().load(image).into(holder.postImage);
+////        }
+////        catch(Exception e) {
+////
+////        }
+////
+////        holder.viewPostButton.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////                Toast.makeText(context, "View Post", Toast.LENGTH_SHORT).show();
+////            }
+////        });
+//
+//
+//        holder.postTitle.setText(data1[position]);
+//        holder.tagOne.setText(data2[position]);
+//        holder.tagTwo.setText(data3[position]);
+//        holder.tagThree.setText(data4[position]);
+//        holder.postImage.setImageResource(images[position]);
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return data1.length;
+//    }
+//
+//    class Holder extends RecyclerView.ViewHolder{
+//
+//        ImageView postImage;
+//        TextView tagOne, tagTwo, tagThree, postTitle;
+//        Button viewPostButton;
+//
+//        public Holder(@NonNull View itemView){
+//            super(itemView);
+//
+//            postImage = itemView.findViewById(R.id.profileRecipeImage);
+//            tagOne = itemView.findViewById(R.id.profileRecipeTag1);
+//            tagTwo = itemView.findViewById(R.id.profileRecipeTag2);
+//            tagThree = itemView.findViewById(R.id.profileRecipeTag3);
+//            postTitle = itemView.findViewById(R.id.profileRecipeTitle);
+//            viewPostButton = itemView.findViewById(R.id.profileRecipeViewPostButton);
+//        }
+//    }
 }
