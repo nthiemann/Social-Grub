b2841dc13b808cfad7d4cb5015aa282720a77a1b
Ricardo Mangandi <Ricardo.mangandi@gmail.com>
2021-03-20 01:40:21 -0400

    Directions Adpater and directions adapter xml did not get uploaded properly.

---
+++ app/src/main/java/com/example/socialgrub/DirectionsAdapter.java
@@ -0,0 +1,76 @@
+package com.example.socialgrub;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class DirectionsAdapter extends RecyclerView.Adapter<DirectionsAdapter.DirectionsViewHolder>  {
+
+
+
+
+    Context context;
+    ArrayList<String> listOfDirectionsFromConfirmPage;
+
+
+    public DirectionsAdapter (Context context, ArrayList<String> listOfDirectionsFromConfirmPage) {
+
+
+        this.context = context;
+        this.listOfDirectionsFromConfirmPage = listOfDirectionsFromConfirmPage;
+    }
+
+    @NonNull
+    @Override
+    public DirectionsAdapter.DirectionsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new DirectionsAdapter.DirectionsViewHolder(LayoutInflater.from(context).inflate(R.layout.directions_adapter,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DirectionsAdapter.DirectionsViewHolder holder, int position) {
+
+
+        holder.directionNth.setText(listOfDirectionsFromConfirmPage.get(position));
+//        holder.directionNthIndex.setText(position + 1);
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listOfDirectionsFromConfirmPage.size();
+    }
+
+
+    class DirectionsViewHolder extends RecyclerView.ViewHolder {
+
+        TextView directionNth;
+        TextView directionNthIndex;
+
+
+        public DirectionsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            directionNth = itemView.findViewById(R.id.adapterDirectionsNameId);
+            directionNthIndex = itemView.findViewById(R.id.NthDirectionId);
+
+        }
+    }
+
+
+
+
+
+
+
+}
